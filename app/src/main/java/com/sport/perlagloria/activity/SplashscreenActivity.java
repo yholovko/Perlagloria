cd4a3ceecbbb709aeca9e3391e178b2f5128bfe4
Jacob <golovko.yakov@gmail.com>
2015-09-28 03:33:24 +0300

    renamed

--- app/src/main/java/com/sport/perlagloria/activity/SplashscreenActivity.java
+++ app/src/main/java/com/sport/perlagloria/activity/SplashscreenActivity.java
@@ -25,7 +25,6 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
-
         customerArrayList = new ArrayList<>();
         loadCustomersInfo();
     }
